30cbf0935e41306ed26c2a24e96edab7f296902d
Théo <theo.dancoisne@outlook.fr>
2022-09-30 23:17:49 +0200

    theo's push, ❤pure JS

--- tools/prefabLoader.js
+++ tools/prefabLoader.js
@@ -1,10 +1,15 @@
-// made by Théo, because i love javascript and i hate manually editing ALL recurring "prefab" and because we don't use php
-async function loadPrefabs() {
+// made by Theo, because I love javascript and I hate manually editing ALL recurring "prefab" and because we don't use php
+/** this is where custom elements are loaded
+ * a custom HTML element need a dash {@link https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements}
+ * to use this script ensure to add jquery (there is one in this directory) and this script in the head for your html file
+ * your custom HTML element must have the same name in the file name in the prefab folder and in the variable below
+ */
+async function loadPrefabs(filenames, location, prefabInPrefab = true) {
     //TODO optimize and make the code clear for anyone
-    function getPrefab(fileName) {
+    async function getPrefab(fileName) {
         return new Promise((resolve, reject) => {
             $.ajax({
-                url: `../prefab/${fileName}.html`,
+                url: `${location}/${fileName}.html`,
                 success: (result) => {
                     resolve(result);
                 },
@@ -14,26 +19,29 @@ async function loadPrefabs() {
             });
         });
     }
-    /** this is where you create your custom elements
-    * a custom HTML element need a dash {@link https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements}
-    * to use this script ensure to add jquery (there is one in this directory) and this script in the head for your html file
-    * your custom HTML element must have the same name in the file name in the prefab folder and in the variable below
-    */
-    let prefab = {
-        "custom-firstNews": null,
-        "custom-RSSfeed": null,
-        "custom-article": null,
-        "custom-interestingNews": null,
-        "custom-themedNews": null,
-        "custom-imagedArticle": null,
-    }
-    for (let key in prefab) {
-        await getPrefab(key).then((result) => {
-            prefab[key] = result;
-        });
+
+    let prefabs = {};
+    filenames.forEach((element) => prefabs[element] = null);
+
+    async function saveContent(key) {
         let elements = document.getElementsByTagName(key);
         Object.values(elements).forEach((item) => {
-            item.innerHTML = prefab[key];
+            item.innerHTML = prefabs[key];
         });
+    }
+
+    for (let key in prefabs) {
+        if (prefabInPrefab) {
+            // if there're prefabs in prefabs we need to load them in order
+            await getPrefab(key).then((result) => {
+                prefabs[key] = result;
+                saveContent(key);
+            });
+        } else {
+            getPrefab(key).then((result) => {
+                prefabs[key] = result;
+                saveContent(key);
+            });
+        }
     }
 };
